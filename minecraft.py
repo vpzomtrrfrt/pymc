7ed978d18424658f807a7ba279282e6248121619
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-05-25 10:22:07 -0600

    Improvement

--- minecraft.py
+++ minecraft.py
@@ -5,6 +5,7 @@ parser.add_argument("--mcdir", dest="mcdir", default=os.path.expanduser("~")+"/.
 parser.add_argument("--username", dest="username", default="Player"+str(random.randrange(1000)))
 parser.add_argument("--game-directory", dest="gamedir")
 parser.add_argument("--access-token", dest="token", default="no")
+parser.add_argument("--uuid", dest="uuid")
 parser.add_argument("--jvm-args", dest="jargs")
 ns = parser.parse_args()
 if ns.gamedir == None:
@@ -13,7 +14,7 @@ platform = sys.platform
 h = open(ns.mcdir+"/versions/"+ns.version+"/"+ns.version+".json")
 j = json.load(h)
 h.close()
-classpath = ns.mcdir+"/versions/"+ns.version+"/"+ns.version+".jar"
+classpath = ""
 nativesdir = ns.mcdir+"/versions/"+ns.version+"/"+ns.version+"-natives-"+str(random.randrange(100000))
 os.mkdir(nativesdir)
 libraries = j["libraries"]
@@ -26,7 +27,7 @@ while "inheritsFrom" in tmp:
         if not k in j:
             j[k] = tmp[k]
     libraries += tmp["libraries"]
-    classpath += ":"+ns.mcdir+"/versions/"+inherit+"/"+inherit+".jar"
+    #classpath += ":"+ns.mcdir+"/versions/"+inherit+"/"+inherit+".jar"
     h.close()
 for l in libraries:
     action = True
@@ -49,11 +50,14 @@ for l in libraries:
                 zp = zipfile.ZipFile(path)
                 zp.extractall(nativesdir)
             else:
-                classpath += ":"+path
+                if len(classpath) > 0:
+                    classpath += ":"
+                classpath += path
         else:
             print("Could not find "+l["name"])
             print(path)
             exit()
+classpath += ":"+ns.mcdir+"/versions/"+ns.version+"/"+ns.version+".jar"
 args = ["java"]
 if ns.jargs != None:
     args += shlex.split(ns.jargs)
@@ -66,8 +70,11 @@ srcargs = {
     "version_name": ns.version,
     "assets_root": ns.mcdir+"/assets",
     "assets_index_name": j["assetIndex"]["id"] if "assetIndex" in j else j["assets"],
-    "version_type": j["type"]
+    "version_type": j["type"],
+    "user_type": "mojang"
 }
+if ns.uuid != None:
+    srcargs["auth_uuid"] = ns.uuid
 for s in spl:
     ss = s.split(" ")
     res = None

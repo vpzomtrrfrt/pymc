b98a8e7690dad841f36bfa2253992144dd595add
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-11-04 20:03:06 -0600

    Windows?

--- minecraft.py
+++ minecraft.py
@@ -11,23 +11,23 @@ ns = parser.parse_args()
 if ns.gamedir == None:
     ns.gamedir = ns.mcdir
 platform = sys.platform
-h = open(ns.mcdir+"/versions/"+ns.version+"/"+ns.version+".json")
+h = open(ns.mcdir+os.sep+"versions"+os.sep+ns.version+os.sep+ns.version+".json")
 j = json.load(h)
 h.close()
 classpath = ""
-nativesdir = ns.mcdir+"/versions/"+ns.version+"/"+ns.version+"-natives-"+str(random.randrange(100000))
+nativesdir = ns.mcdir+os.sep+"versions"+os.sep+ns.version+os.sep+ns.version+"-natives-"+str(random.randrange(100000))
 os.mkdir(nativesdir)
 libraries = j["libraries"]
 tmp = j
 while "inheritsFrom" in tmp:
     inherit = tmp["inheritsFrom"]
-    h = open(ns.mcdir+"/versions/"+inherit+"/"+inherit+".json")
+    h = open(ns.mcdir+os.sep+"versions"+os.sep+inherit+os.sep+inherit+".json")
     tmp = json.load(h)
     for k in tmp:
         if not k in j:
             j[k] = tmp[k]
     libraries += tmp["libraries"]
-    #classpath += ":"+ns.mcdir+"/versions/"+inherit+"/"+inherit+".jar"
+    #classpath += ":"+ns.mcdir+"/versions/"+inherit+os.sep+inherit+".jar"
     h.close()
 for l in libraries:
     action = True
@@ -41,7 +41,7 @@ for l in libraries:
                 action = rule["action"]=="allow"
     if action:
         spl = l["name"].split(":")
-        path = ns.mcdir+"/libraries/"+spl[0].replace(".", "/")+"/"+spl[1]+"/"+spl[2]+"/"+spl[1]+"-"+spl[2]
+        path = ns.mcdir+os.sep+"libraries"+os.sep+spl[0].replace(".", os.sep)+os.sep+spl[1]+os.sep+spl[2]+os.sep+spl[1]+"-"+spl[2]
         if "natives" in l:
             path += "-"+l["natives"][platform]
         path += ".jar"
@@ -57,7 +57,7 @@ for l in libraries:
             print("Could not find "+l["name"])
             print(path)
             exit()
-classpath += ":"+ns.mcdir+"/versions/"+ns.version+"/"+ns.version+".jar"
+classpath += ":"+ns.mcdir+os.sep+"versions"+os.sep+ns.version+os.sep+ns.version+".jar"
 args = ["java"]
 if ns.jargs != None:
     args += shlex.split(ns.jargs)
@@ -68,7 +68,7 @@ srcargs = {
     "auth_access_token": ns.token,
     "game_directory": ns.gamedir,
     "version_name": ns.version,
-    "assets_root": ns.mcdir+"/assets",
+    "assets_root": ns.mcdir+os.sep+"assets",
     "assets_index_name": j["assetIndex"]["id"] if "assetIndex" in j else j["assets"],
     "version_type": j["type"],
     "user_type": "mojang"
